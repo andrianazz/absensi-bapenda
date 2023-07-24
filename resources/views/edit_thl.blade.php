7b7e5659db6447d897f530b915d1d0cbb6f9fe98
andrianazz <andrianwahyu41@gmail.com>
2023-07-24 20:32:32 +0700

    change edit pass

--- resources/views/edit_thl.blade.php
+++ resources/views/edit_thl.blade.php
@@ -91,7 +91,13 @@
                         </div>
                     </div>
 
-                    <div class="d-flex flex-column align-items-end">
+                    <div class="d-flex flex-row justify-content-between">
+                        <a href="/edit-pass-user/{{$user->id}}" class="btn icon icon-left btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle">
+                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                        </svg> Edit Pass
+                        </a>
+
                         <button type="submit" class="btn icon icon-left btn-success "><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle">
                                 <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
                                 <polyline points="22 4 12 14.01 9 11.01"></polyline>
--- resources/views/thl.blade.php
+++ resources/views/thl.blade.php
@@ -129,10 +129,10 @@
                                         <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
                                     </svg> Edit
                                 </a>
-                                <a href="/edit-pass-user/{{$user->id}}" class="btn icon icon-left btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle">
+                                {{-- <a href="/edit-pass-user/{{$user->id}}" class="btn icon icon-left btn-warning"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check-circle">
                                     <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
                                     <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                </svg> Edit Pass
+                                </svg> Edit Pass --}}
                             </a>
                                 <button type="button" data-bs-toggle="modal" data-bs-target="#danger{{$user->id}}" class="btn icon icon-left btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-alert-circle">
                                         <circle cx="12" cy="12" r="10"></circle>
